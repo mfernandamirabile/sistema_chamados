261f7e7bd24c5a4fec1b40ad4652278f302c3643
mfernandamirabile <fernanda_mirabile@hotmail.com>
2023-12-05 19:32:06 -0300

    mudança de imgs

--- imagens.py
+++ imagens.py
@@ -6,8 +6,8 @@ from PIL import Image, ImageTk
 user = 'ferna'
 x = (70,50)
 
-imagem1 = Image.open(f'C:/Users/{user}/cameras/imgs/Cartao.png').resize(x)
-imagem2 = Image.open(f'C:/Users/{user}/cameras/imgs/Movimento.png').resize(x)
-imagem3 = Image.open(f'C:/Users/{user}/cameras/imgs/Titular.png').resize(x)
-imagem4 = Image.open(f'C:/Users/{user}/cameras/imgs/Historico.png').resize(x)
-imagem5 = Image.open(f'C:/Users/{user}/cameras/imgs/Estatistica.png').resize(x)
+imagem1 = Image.open(f'C:/Users/{user}/sistema_chamados/imgs/acesso.png').resize(x)
+imagem2 = Image.open(f'C:/Users/{user}/sistema_chamados/imgs/solicitacao.png').resize(x)
+imagem3 = Image.open(f'C:/Users/{user}/sistema_chamados/imgs/Titular.png').resize(x)
+imagem4 = Image.open(f'C:/Users/{user}/sistema_chamados/imgs/relatorio.png').resize(x)
+imagem5 = Image.open(f'C:/Users/{user}/sistema_chamados/imgs/dashboard.png').resize(x)
